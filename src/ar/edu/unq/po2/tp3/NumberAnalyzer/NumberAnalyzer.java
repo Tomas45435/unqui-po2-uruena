21195d9a79a78814e80030606b6b421ca26b2c25
tomas <tomasuruena74@gmail.com>
2025-04-12 11:12:42 -0300

    Nuevo metodo Maximo Comun Multiplo

--- src/ar/edu/unq/po2/tp3/NumberAnalyzer/NumberAnalyzer.java
+++ src/ar/edu/unq/po2/tp3/NumberAnalyzer/NumberAnalyzer.java
@@ -3,13 +3,13 @@ package ar.edu.unq.po2.tp3.NumberAnalyzer;
 
 public class NumberAnalyzer {
 	
-	/**
+	/*
      * Encuentra el número con la mayor cantidad de dígitos pares en un arreglo de numeros enteros.
      * @return El número con más dígitos pares. Si hay empate, devuelve el primero encontrado.
      *         Si el arreglo está vacío, devuelve 0.
      */
 	
-public int encontrarNumeroConMasDigitosPares(int[] arregloNumeros) {
+	public static int numeroConMasDigitosPares(int[] arregloNumeros) {
 		
 		// Verifico si el arreglo esta vacio.
 		if (arregloNumeros == null || arregloNumeros.length == 0) {
@@ -37,7 +37,7 @@ public int encontrarNumeroConMasDigitosPares(int[] arregloNumeros) {
      * @return Cantidad de dígitos pares
     */
 
-	public int contarDigitosPares(int numero) {
+	public static int contarDigitosPares(int numero) {
 		
 		// Si el numero es 0, se retorna 1 ya que 0 se considera par
 		if (numero == 0) {
@@ -58,6 +58,55 @@ public int encontrarNumeroConMasDigitosPares(int[] arregloNumeros) {
 		
 		return contador;
 		
+	}
+
+	
+	// Retorna el minimo comun multiplo
+	public static int mcm(int num1, int num2) {
+	
+		int a = Math.max(num1, num2);
+		int b = Math.min(num1, num2);
+		int resultado = (a / mcd(num1, num2)) * b;
+	
+		return resultado;
+	
+	}
+	
+	// Retorna el maximo comun divisor
+	public static int mcd(int num1, int num2) {
+	
+		int a = Math.max(num1, num2);
+		int b = Math.min(num1, num2);
+	
+		int resultado = 0;
+		do {
+			resultado = b;
+			b = a % b;
+			a = resultado;
+		} while (b != 0);
+	
+		return resultado;
+	}
+	
+	
+	public static int maximoComunMultiplo(int num1, int num2) {
+		
+        if (num1 == 0 || num2 == 0) {
+            return 0; // El único múltiplo común de 0 es 0
+        }
+
+		int rangoMax = 1000;
+		int rangoMin = 0;
+		int mcm = mcm(num1, num2);
+		int mayorMultiplo = (rangoMax / mcm) * mcm;
+		
+		
+		// Se verifica si el multiplo comun esta dentro del rango.
+		if (mayorMultiplo > rangoMin && mayorMultiplo <= rangoMax) {
+			return mayorMultiplo;
+		} else {									//Si el multiplo es menor que el rango minimo devuelve -1.
+			return -1;
+		}
 	}
 }
 
--- test/ar/edu/unq/po2/tp3/NumberAnalyzer/test/NumberAnalyzerTest.java
+++ test/ar/edu/unq/po2/tp3/NumberAnalyzer/test/NumberAnalyzerTest.java
@@ -16,14 +16,34 @@ class NumberAnalyzerTest {
 	@Test
 	public void testCantidadDeDigitosPares() {
 		
-		NumberAnalyzer numberAnalyzer = new NumberAnalyzer();
 		int[] numeros = new int[] {1234, 2468, 13579, 86420, 5555, 22111} ;
 		
 		//Obtengo el numero con mas digitos pares
-			int numMaxDigitosPares = numberAnalyzer.encontrarNumeroConMasDigitosPares(numeros);
+			int numMaxDigitosPares = NumberAnalyzer.numeroConMasDigitosPares(numeros);
 			
 		//Verifico que el numero es el esperado.
 			assertEquals(numMaxDigitosPares, 86420);
+	}
+	
+	
+	@Test
+	public void testMaximoComunMultiploDentroDeRango() {
+		
+		//Obtengo el maximo comun multiplo dentro un rango
+		int maxComunMultiplo = NumberAnalyzer.maximoComunMultiplo(3, 9);
+		
+		//Verifico que el numero es el esperado.
+		assertEquals(maxComunMultiplo, 999);
+	}
+	
+	@Test
+	public void testMaximoComunMultiploFueraDeRango() {
+		
+		//Obtengo el maximo comun multiplo dentro un rango, en este caso es fuera de rango
+		int maxComunMultiplo = NumberAnalyzer.maximoComunMultiplo(1001, 1);
+		
+		//Verifico que devuelva fuera de rango.
+		assertEquals(maxComunMultiplo, -1);
 	}
 
 
