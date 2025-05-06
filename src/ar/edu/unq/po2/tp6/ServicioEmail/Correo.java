2a14d123f4e8b69014b4f0539feb0f75fdaeb42e
tomas <tomasuruena74@gmail.com>
2025-05-06 17:57:07 -0300

    Punto 1 TP SOLID

---
+++ src/ar/edu/unq/po2/tp6/ServicioEmail/Correo.java
@@ -0,0 +1,5 @@
+package ar.edu.unq.po2.tp6.ServicioEmail;
+
+public class Correo {
+
+}
---
+++ src/ar/edu/unq/po2/tp6/ServicioEmail/IServidor.java
@@ -0,0 +1,6 @@
+package ar.edu.unq.po2.tp6.ServicioEmail;
+
+public interface IServidor {
+	
+	public void conectar(String nombreUsuario, String passusuario);
+}
